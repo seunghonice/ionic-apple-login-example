c1c234e8b341659680b8e39e8748b23fce0b5794
Seungho Hong <seunghonice@gmail.com>
2022-02-10 00:04:09 +0900

    first commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { Plugins } from '@capacitor/core'
+import {isPlatform} from '@ionic/react'
+
+
+
+const { Device } = Plugins
 
 const Tab1: React.FC = () => {
+  async function signIn() {
+
+  }
+  async function signInWithApple() {
+    const { SignInWithApple } = Plugins
+    SignInWithApple.Authorize()
+    .then((response: any) => {
+      console.log(response)
+    }).catch((response: any) => {
+      console.error(response)
+    })
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -11,15 +29,18 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={signInWithApple}>Login With Apple</IonButton>
+        <div onClick={signInWithApple}>LoginWithApple</div>
+    </IonContent>
+  </IonPage>
+  )
+}
+
 
 export default Tab1;
